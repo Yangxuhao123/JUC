834befb0f642a2ea0847993ed0df3e86402267c2
DreamWaking <m18751970909@163.com>
2023-02-25 15:08:23 +0800

    微服务注册和心跳

---
+++ src/main/java/com/juc/demo/HeartbeatRequest.java
@@ -0,0 +1,22 @@
+package com.juc.demo;
+
+/**
+ * 心跳请求
+ * @author zhonghuashishan
+ *
+ */
+public class HeartbeatRequest {
+
+	/**
+	 * 服务实例id
+	 */
+	private String serviceInstanceId;
+
+	public String getServiceInstanceId() {
+		return serviceInstanceId;
+	}
+	public void setServiceInstanceId(String serviceInstanceId) {
+		this.serviceInstanceId = serviceInstanceId;
+	}
+	
+}
